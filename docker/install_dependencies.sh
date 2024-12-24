17cbc692442bdefe22505966894f28ac6ebfe020
Satish Natarajan <natarajan.sa@northeastern.edu>
2024-12-24 14:11:34 -0500

    Added builder file and install script

---
+++ docker/install_dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# exit on errors
+set -e
+
+# Get ubuntu distribution code name. All STOL APT debian packages are pushed to S3 bucket based on distribution codename.
+. /etc/lsb-release
+
+# add the STOL APT repository
+sudo echo "deb [trusted=yes] http://s3.amazonaws.com/stol-apt-repository ${DISTRIB_CODENAME} main" > /etc/apt/sources.list.d/stol-apt-repository.list
+
+apt-get update
