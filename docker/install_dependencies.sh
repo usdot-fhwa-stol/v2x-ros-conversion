00a75387cf9475052e7cce410ce21167fb55de2b
Satish Natarajan <natarajan.sa@northeastern.edu>
2025-01-31 12:41:24 -0500

    Updated merge conflicts

--- docker/install_dependencies.sh
+++ docker/install_dependencies.sh
@@ -7,15 +7,9 @@ set -e
 . /etc/lsb-release
 
 # add the STOL APT repository
-<<<<<<< HEAD
 echo "deb [trusted=yes] http://s3.amazonaws.com/stol-apt-repository ${DISTRIB_CODENAME} main" | sudo tee /etc/apt/sources.list.d/stol-apt-repository.list
 
 sudo apt-get update
 
 # install all things needed for deployment, always done
 sudo apt-get install -y carma-j2735-1
-=======
-sudo echo "deb [trusted=yes] http://s3.amazonaws.com/stol-apt-repository ${DISTRIB_CODENAME} main" > /etc/apt/sources.list.d/stol-apt-repository.list
-
-apt-get update
->>>>>>> 17cbc69 (Added builder file and install script)
